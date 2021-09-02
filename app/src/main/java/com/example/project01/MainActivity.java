04450013677d212001317ea1f5270a89c236492c
Hernan Hernandez <hernanhg10@gmail.com>
2021-09-01 20:12:35 -0700

    added user register page

--- app/src/main/java/com/example/project01/MainActivity.java
+++ app/src/main/java/com/example/project01/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //buttons
+    private EditText username, password;
+    private Button loginBtn, regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        regBtn = findViewById(R.id.regBtn);
+
+        // register data in room first
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
