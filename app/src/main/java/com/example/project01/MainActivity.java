fbee95837cd14fcf997aa0c103c4e9717365d55d
Oscar Ruiz <44891178+Xriginal@users.noreply.github.com>
2021-09-08 21:06:01 -0700

    updated

--- app/src/main/java/com/example/project01/MainActivity.java
+++ app/src/main/java/com/example/project01/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Account Created for testing purposes
+ * Username: abc
+ * Password: 123
+ */
+
 package com.example.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText name, pass;
     private Button loginBtn, regBtn;
 
-    private String test1 = "test1", test2 = "test2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,44 +29,59 @@ public class MainActivity extends AppCompatActivity {
 
         name = findViewById(R.id.username);
         pass = findViewById(R.id.password);
+
         loginBtn = findViewById(R.id.loginBtn);
         regBtn = findViewById(R.id.regBtn);
 
-        // register data in room first
+        // register button
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
 
+        // login
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 // doesnt work the pop up message aint doing its job
                 String userField = name.getText().toString();
                 String passField = pass.getText().toString();
-                if(userField.equals(test1) && passField.equals(test2))
-                {
-                    //landing page doesnt exist yet but the login should work
-                    //did a test user and pass
-                    Intent intent = new Intent(MainActivity.this, LandingPage.class);
-                    startActivity(intent);
-                }
-                if(userField.isEmpty() && passField.isEmpty())
-                {
+
+                if (userField.isEmpty() && passField.isEmpty()) {
                     Toast toast = Toast.makeText(getApplicationContext(), "Password and Username is empty", Toast.LENGTH_LONG);
                     toast.show();
                 }
-                //makayla landpage
-              //  Intent intent = new Intent(MainActivity.this, LandingPage.class);
-
+                // get user from database/do query
+                else {
+                    UserDatabase userDatabase = UserDatabase.getUserDatabase(getApplicationContext());
+                    final UserDao userDao = userDatabase.userDao();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            UserEntity userEntity = userDao.login(userField, passField);
+                            if (userEntity == null) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // do error message
+                                        Toast.makeText(getApplicationContext(), "does not match user", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            } else {
+                                // match
+                                String username = userEntity.username;
+                                Intent intent = new Intent(MainActivity.this, LandingPage.class)
+                                        .putExtra("username", username);
+                                startActivity(intent);
+                            }
+                        }
+                    }).start();
+                }
             }
         });
-
     }
-
 }
