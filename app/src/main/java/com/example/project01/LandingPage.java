dd841489857571d12c555dcf908449795f16da5d
Oscar Ruiz <44891178+Xriginal@users.noreply.github.com>
2021-09-08 22:23:13 -0700

    updating userprofile

--- app/src/main/java/com/example/project01/LandingPage.java
+++ app/src/main/java/com/example/project01/LandingPage.java
@@ -2,21 +2,36 @@ package com.example.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LandingPage extends AppCompatActivity {
 
     private TextView userDisplay;
+    Button usrProfileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
+        usrProfileBtn = findViewById(R.id.userProfile);
         userDisplay = findViewById(R.id.userDisplay);
 
+        // test for user sign in
         String userOutput = getIntent().getStringExtra("username");
         userDisplay.setText(userOutput);
+
+        // User profile button
+        usrProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPage.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/project01/UserProfile.java
+++ app/src/main/java/com/example/project01/UserProfile.java
@@ -3,9 +3,17 @@ package com.example.project01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserProfile extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private EditText new_pass;
+    private EditText new_user;
+    private Button edit;
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
