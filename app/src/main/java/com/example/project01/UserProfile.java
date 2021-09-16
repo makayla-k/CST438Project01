56aac82fd0e21114d4d66999ae23fc47eb32bb2d
Oscar Ruiz <44891178+Xriginal@users.noreply.github.com>
2021-09-15 22:01:22 -0700

    updating userProfile

--- app/src/main/java/com/example/project01/UserProfile.java
+++ app/src/main/java/com/example/project01/UserProfile.java
@@ -7,29 +7,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserProfile extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
-    private EditText new_pass;
-    private EditText new_user;
     private Button edit;
     private Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
         username = findViewById(R.id.current_name);
         password = findViewById(R.id.current_pass);
         home = findViewById(R.id.home);
+        edit = findViewById(R.id.edit_profile);
 
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UserProfile.this, LandingPage.class);
-                startActivity(intent);
+       // String userData = userDao.getStringExtra(username);
+        //String passData = UserDao.getStringExtra(password)
+        // how do I recieve information to see if it displays on the screen or nah
+        // do i use log in
+
+        edit.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        final UserEntity news = new UserEntity();
+                                        news.setUsername(username.getText().toString());
+                                        news.setPassword(password.getText().toString());
+                                        UserDatabase userDatabase = UserDatabase.getUserDatabase(getApplicationContext());
+                                        final UserDao newIdentiy = userDatabase.userDao();
+                                        new Thread(() ->
+                                        {
+                                            // change name
+                                            //not sure what to put here
+                                         //   newIdentiy.registerUser(news);
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    Intent intent = new Intent(UserProfile.this, LandingPage.class);
+                                                    startActivity(intent);
+                                                }
+                                            });
+                                        }).start();
+                                    }
+                                });
+
+                home.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(UserProfile.this, LandingPage.class);
+                        startActivity(intent);
+                    }
+                });
             }
-        });
-    }
 }
